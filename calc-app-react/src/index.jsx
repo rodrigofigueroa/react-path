269711dbd828d75b017566d0fb2ad5b7de2d443f
rodrigo <olivertate274@gmail.com>
2026-02-02 12:54:42 -0600

    adding app js and debugging

--- calc-app-react/src/index.jsx
+++ calc-app-react/src/index.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App'
 
 
 const root = ReactDOM.createRoot( document.getElementById( 'root' ) )
 
-root.render( <h1>React is deprecated</h1>)
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+root.render(
+  <React.StrictMode>
+   <App /> 
+  </React.StrictMode>
+)
