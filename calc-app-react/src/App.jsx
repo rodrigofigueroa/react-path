502976f244da54121e361c95502af8cdfb17c7b7
rodrigo <olivertate274@gmail.com>
2026-02-04 19:03:31 -0600

    adding calculator

--- calc-app-react/src/App.jsx
+++ calc-app-react/src/App.jsx
@@ -1,10 +1,50 @@
 import React from 'react'
-import './App.css';
+import './App.css'
+import './Calc.css'
 
 const App = () => (
-  <section class="container">
+  <main className="container">
     <h1>Calc APP</h1>
-  </section>
+    <section className="con-calculator">
+      <section className="input-text">
+
+      </section>
+      <section className='cont-1'>
+          <section className="numbers">
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>0</button>
+          </section>
+
+          <section className="functions">
+              <button className='round-a'>Limpiar</button>
+              <button>Borrar</button>
+          </section>
+
+      </section>
+
+      <section className='cont-2'>
+
+        <section className="math-operators">      
+          <button>+</button>
+          <button>-</button>
+          <button>*</button>
+          <button>/</button>
+          <button className='round-b'>=</button>
+
+        </section>
+      </section>
+
+      
+    </section>
+  </main>
 )
 
 export default App
